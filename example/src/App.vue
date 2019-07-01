d5f0e0f775dc72b965b39ced736c7a004998b72a
Ian Johnson <ian@ianjohnson.net>
2019-07-01 13:32:42 -0700

    Added a working example, finally 🙄

---
+++ example/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <vue-documenter>
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </vue-documenter>
+  </div>
+</template>
+
+<script>
+import VueDocumenter from './components/VueDocumenter.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    VueDocumenter,
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
